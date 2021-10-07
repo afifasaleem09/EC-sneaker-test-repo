97c2b2be614f454d3afe7a66c1c311b46eea615a
afifasaleem09 <90034557+afifasaleem09@users.noreply.github.com>
2021-10-07 18:21:10 +0500

    Add files via upload

--- Cart.js
+++ Cart.js
@@ -1,67 +1,71 @@
-const cartModel = require('../models/Cart')
 
-class Cart {
-  constructor(oldCart) {
-    this.items = oldCart.items || {};
-    this.totalQty = oldCart.totalQty || 0;
-    this.totalPrice = oldCart.totalPrice || 0;
-    this.userId = oldCart.userId || "";
-  }
+const mongoose = require('mongoose')
 
-  add(item, id) {
-    let storedItem = this.items[id];
-    if (!storedItem) {
-      storedItem = this.items[id] = { item: item, qty: 0, price: 0 };
-    }
-    storedItem.qty++;
-    storedItem.price = parseFloat((storedItem.item.price * storedItem.qty).toFixed(2));
-    this.items[id]=storedItem
-    this.totalQty++;
-    this.totalPrice += storedItem.item.price;
-    this.totalPrice = parseFloat(this.totalPrice.toFixed(2))
-    return this
+CartSchema = mongoose.Schema({
+  items: {
+    type: Object
+  },
+  totalQty: {
+    type: Number
+  },
+  totalPrice: {
+    type: Number
+  },
+  userId: {
+    type: String
   }
+})
 
-  generateModel(){
-    let newCart = new cartModel({
-      items: this.items,
-      totalQty: this.totalQty,
-      totalPrice: this.totalPrice,
-      userId: this.userId
-    })
-    return newCart
-  }
+var Cart = module.exports = mongoose.model('Cart', CartSchema)
 
-  decreaseQty(id) {
-    this.items[id].qty--;
-    this.items[id].price -= this.items[id].item.price;
-    this.items[id].price = parseFloat(this.items[id].price.toFixed(2))
-    this.totalQty--;
-    this.totalPrice -= this.items[id].item.price
-    this.totalPrice = parseFloat(this.totalPrice.toFixed(2))
-    if (this.items[id].qty <= 0) {
-      delete this.items[id];
-    }
-    return this
-  }
-
-  increaseQty(id) {
-    this.items[id].qty++;
-    this.items[id].price += this.items[id].item.price;
-    this.items[id].price = parseFloat(this.items[id].price.toFixed(2))
-    this.totalQty++;
-    this.totalPrice += this.items[id].item.price
-    this.totalPrice = parseFloat(this.totalPrice.toFixed(2))
-    return this
-  }
-
-  generateArray() {
-    let arr = [];
-    for (let id in this.items) {
-      arr.push(this.items[id])
-    }
-    return arr;
-  }
+module.exports.getCartByUserId = function (uid, callback) {
+  let query = { userId: uid }
+  Cart.find(query, callback)
 }
 
-module.exports = Cart
+module.exports.getCartById = function (id, callback) {
+  Cart.findById(id, callback)
+}
+
+module.exports.updateCartByUserId = function (userId, newCart, callback) {
+  let query = { userId: userId }
+  Cart.find(query, function (err, c) {
+    if (err) throw err
+
+    //exist cart in databse
+    if (c.length > 0) {
+      Cart.findOneAndUpdate(
+        { userId: userId },
+        {
+          $set: {
+            items: newCart.items,
+            totalQty: newCart.totalQty,
+            totalPrice: newCart.totalPrice,
+            userId: userId
+          }
+        },
+        { new: true },
+        callback
+      )
+    } else {
+      //no cart in database
+      newCart.save(callback)
+    }
+  })
+}
+
+module.exports.updateCartByCartId = function (cartId, newCart, callback) {
+  Cart.findById(
+    { _id: cartId },
+    {
+      $set: newCart
+    },
+    callback
+  )
+}
+
+
+
+module.exports.createCart = function (newCart, callback) {
+  newCart.save(callback)
+}
