132865416705a7e4d1c18af9de0a284d4435a560
afifasaleem09 <90034557+afifasaleem09@users.noreply.github.com>
2021-10-07 18:06:16 +0500

    routes

--- index.js
+++ index.js
@@ -4,18 +4,84 @@
  ** Github URL: https://github.com/quintuslabs/fashion-cube
  */
 
-import rootReducer from "../reducers";
-import { createStore, applyMiddleware, compose } from "redux";
-import thunk from "redux-thunk";
+import React, { Component } from "react";
+import {
+  BrowserRouter as Router,
+  Redirect,
+  Route,
+  Switch
+} from "react-router-dom";
+import { registerNav } from "../modules/Navigation";
+import { createBrowserHistory } from "history";
+import PageNotFound from "../views/PageNotFound";
+import HomeRoutes from "./HomeRoutes";
+import PrivateRoutes from "./PrivateRoutes";
+import Auth from "../modules/Auth";
 
-const initialState = {};
-const middlewares = [thunk];
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const PrivateRouter = ({ component, ...options }) => {
+  const finalComponent =
+    Auth.getUserDetails() !== undefined &&
+    Auth.getUserDetails() !== null &&
+    Auth.getToken() !== undefined ? (
+      <Route {...options} component={component} />
+    ) : (
+      <Redirect to="/PageNotFound" />
+    );
 
-const store = createStore(
-  rootReducer,
-  initialState,
-  composeEnhancers(applyMiddleware(...middlewares))
-);
+  return finalComponent;
+};
 
-export default store;
+class Routes extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    const browserHistory = createBrowserHistory();
+
+    return (
+      <div>
+        <Router ref={registerNav}>
+          <Switch>
+            {HomeRoutes.map((homeRoute, index) => {
+              return (
+                <Route
+                  key={index}
+                  path={homeRoute.path}
+                  exact={homeRoute.exact}
+                  component={props => {
+                    return (
+                      <homeRoute.layout {...props}>
+                        <homeRoute.component {...props} />
+                      </homeRoute.layout>
+                    );
+                  }}
+                />
+              );
+            })}
+            {PrivateRoutes.map((privateRoute, index) => {
+              return (
+                <PrivateRouter
+                  key={index}
+                  path={privateRoute.path}
+                  exact={privateRoute.exact}
+                  component={props => {
+                    return (
+                      <privateRoute.layout {...props}>
+                        <privateRoute.component {...props} />
+                      </privateRoute.layout>
+                    );
+                  }}
+                />
+              );
+            })}
+            <Route Redirect to="/PageNotFound" exact component={PageNotFound} />
+          </Switch>
+        </Router>
+      </div>
+    );
+  }
+}
+
+export default Routes;
